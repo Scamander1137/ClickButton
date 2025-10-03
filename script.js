cd9d8bc369167f0363eaf0dca8538e716c7c51b0
Scamander1137 <=>
2025-10-03 15:41:20 +0700

    03/10/2025

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const button = document.getElementById("button");
 const counter = document.getElementById("counter");
 
-let i = 0;
+let i = localStorage.getItem("counter") || 0;
+counter.textContent = i;
 
 button.addEventListener("click", () => {
   i++;
   counter.textContent = i;
+  localStorage.setItem("counter", i);
 });
